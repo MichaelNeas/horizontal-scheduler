5720a3563b35aee63dd08fb1fdf8a1b8fb40ae1c
michaelneas <michael.r.neas@gmail.com>
2019-02-08 23:19:08 -0500

    adds the beginning of the horizontal scrolly boy

--- horizontalscheduler/ViewController.swift
+++ horizontalscheduler/ViewController.swift
@@ -9,12 +9,40 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    lazy var scrollView: UIScrollView = {
+        let view = UIScrollView()
+        view.translatesAutoresizingMaskIntoConstraints = false
+        view.contentSize.width = 24000
+        view.backgroundColor = UIColor.lightGray
+        return view
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        view.addSubview(scrollView)
+        setupScrollView()
+        
+        
     }
-
-
+    
+    func setupScrollView() {
+        scrollView.topAnchor.constraint(equalTo: view.topAnchor).isActive = true
+        scrollView.bottomAnchor.constraint(equalTo: view.bottomAnchor).isActive = true
+        scrollView.leftAnchor.constraint(equalTo: view.leftAnchor).isActive = true
+        scrollView.rightAnchor.constraint(equalTo: view.rightAnchor).isActive = true
+        setupObjects()
+    }
+    
+    func setupObjects() {
+        for _ in 0..<100 {
+            let rndX = Int.random(in: 0..<24000)
+            let rndY = Int.random(in: 0..<Int(view.bounds.maxY))
+            let rndWidth = Int.random(in: 0..<1000)
+            let space = UIView(frame: CGRect(x: rndX, y: rndY, width: rndWidth, height: 100))
+            space.backgroundColor = UIColor(hue: CGFloat(drand48()), saturation: 1, brightness: 1, alpha: 1)
+            scrollView.addSubview(space)
+        }
+    }
 }
-
